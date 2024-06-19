14a74814f7362f3fe27f1ea7a1c07df7d528c1b9
Bryce <brycecyb@gmail.com>
2024-06-19 18:36:13 +0800

    Remove heightForRowAt function

--- DateFormatterDemo/ViewController.swift
+++ DateFormatterDemo/ViewController.swift
@@ -161,8 +161,4 @@ extension ViewController: UITableViewDataSource {
         
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 260
-    }
 }
